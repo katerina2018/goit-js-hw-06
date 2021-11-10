a861f28be07867a8fcf9f6ba03ad630f60bec8da
katerina2018 <tvorchestvo15@gmail.com>
2021-11-10 14:17:07 +0200

    commit

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,21 @@
 const inputEl = document.querySelector('#validation-input');
-const checkEl = inputEl.getAttribute('data-length');
+const checkEl = Number(inputEl.getAttribute('data-length'));
 
 inputEl.addEventListener('blur', () => {
-    if (inputEl.value.length >= checkEl) {
-        inputEl.classList.add('valid');
-        inputEl.classList.remove('invalid');
+    // вариант 1
+    // inputEl.classList.add('invalid');
+    // if (inputEl.value.length === checkEl) {
+    //     inputEl.classList.replace('invalid', 'valid');
+    // }
+    // вариант 2
+    if (inputEl.value.length === checkEl) {
+        changeClass(inputEl, 'valid', 'invalid');
     } else {
-        inputEl.classList.add('invalid');
-        inputEl.classList.remove('valid');
+        changeClass(inputEl, 'invalid', 'valid');
     }
 });
+
+function changeClass(elem, classAdd, classRemove) {
+    elem.classList.add(classAdd);
+    elem.classList.remove(classRemove);
+}
